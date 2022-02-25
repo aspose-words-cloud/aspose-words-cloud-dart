dcbdc7fd6f45d98bd0d8c29cc56853b8c8fd3b09
jenkins <jenkins.aspose@gmail.com>
2022-02-24 23:57:31 -0600

    SDK regenerated by CI server [ci skip]

--- lib/src/configuration.dart
+++ lib/src/configuration.dart
@@ -33,8 +33,10 @@ class Configuration {
   final String baseUrl;
   final bool debugMode;
   final Duration timeout;
+  final String rsaExponent;
+  final String rsaModulus;
 
-  Configuration(final this.clientId, final this.clientSecret, {final this.baseUrl = 'https://api.aspose.cloud', final this.debugMode = false, final this.timeout = const Duration(seconds: 300)});
+  Configuration(final this.clientId, final this.clientSecret, {final this.baseUrl = 'https://api.aspose.cloud', final this.debugMode = false, final this.timeout = const Duration(seconds: 300), final this.rsaExponent, final this.rsaModulus});
 
   Configuration.fromJson(final Map<String, dynamic> json)
     : clientId = json['ClientId'] as String,
@@ -42,6 +44,8 @@ class Configuration {
       baseUrl = json['BaseUrl'] as String ?? 'https://api.aspose.cloud',
       debugMode = json['DebugMode'] as bool,
       timeout = Duration(seconds: json['Timeout'] as int ?? 300);
+      rsaExponent = json['RsaExponent'] as String,
+      rsaModulus = json['RsaModulus'] as String;
 
   String getApiRootUrl()
   {
