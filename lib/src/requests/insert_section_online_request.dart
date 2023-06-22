18e624aed5a39fe18e904cb07f7e8373e29a5726
jenkins <jenkins.aspose@gmail.com>
2023-06-22 07:14:00 -0500

    SDK regenerated by CI server [ci skip]

--- lib/src/requests/insert_section_online_request.dart
+++ lib/src/requests/insert_section_online_request.dart
@@ -114,7 +114,7 @@ class InsertSectionOnlineRequest implements RequestBase {
     }
     var _url = _apiClient.configuration.getApiRootUrl() + _apiClient.applyQueryParams(_path, _queryParams).replaceAll('//', '/');
     var _body = _apiClient.serializeBodyParts(_bodyParts, _headers);
-    return ApiRequestData('POST', _url, _headers, _body);
+    return ApiRequestData('PUT', _url, _headers, _body);
   }
 
   @override
