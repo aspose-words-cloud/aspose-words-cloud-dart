3c5b9bdb497d12725e3631d4b953ab03c704d95f
jenkins <jenkins.aspose@gmail.com>
2026-01-16 09:56:16 -0600

    SDK regenerated by CI server [ci skip]

--- lib/src/models/gif_save_options_data.dart
+++ lib/src/models/gif_save_options_data.dart
@@ -1,7 +1,7 @@
 /*
  * --------------------------------------------------------------------------------
  * <copyright company="Aspose" file="gif_save_options_data.dart">
- *   Copyright (c) 2025 Aspose.Words for Cloud
+ *   Copyright (c) 2026 Aspose.Words for Cloud
  * </copyright>
  * <summary>
  *   Permission is hereby granted, free of charge, to any person obtaining a copy
